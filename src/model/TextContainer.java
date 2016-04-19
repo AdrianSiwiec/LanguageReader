38dba64e1ff29a30b596dcea3ffd455f9dd7ccda
pierrehuitieme <adrian.siwiec@gmail.com>
2016-04-19 13:30:54 +0200

    Created MVC pattern

---
+++ src/model/TextContainer.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.io.File;
+
+/**
+ * Created by pierre on 19/04/16.
+ */
+public class TextContainer {
+    public TextContainer(File file) {
+        System.out.println(file.toString());
+    }
+}
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package view;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -6,9 +6,21 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import model.TextContainer;
 
-public class HelloWorld extends Application {
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+/**
+ * Created by pierre on 19/04/16.
+ */
+public class Main extends Application {
+    private Desktop desktop = Desktop.getDesktop();
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -18,11 +30,18 @@ public class HelloWorld extends Application {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
+
+        FileChooser fileChooser = new FileChooser();
+
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("A");
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if(file!=null) {
+                        new TextContainer(file);
+                }
             }
         });
 
